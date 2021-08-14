7b5f55f3fccdc8e0fb37c3c174fdef07bf612ca9
ew0345 <ew0345@users.noreply.github.com>
2021-08-14 09:40:07 -0400

    Kotlin Version 1.1.0
    Kotlin version of StringGen, includes all the same features and works the same way.

--- StringGen.kt
+++ StringGen.kt
@@ -1,7 +1,10 @@
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
 import kotlin.random.Random
 import kotlin.system.exitProcess
 
-const val version = "1.0.0"
+const val version = "1.1.0"
 
 val letters = arrayOf("A","B","C","D","E","F","G","H","I","J","K","L",
     "M","N","O","P","Q","R","S","T","U","V","W","X", "Y","Z","a","b",
@@ -17,7 +20,8 @@ fun displayInfo() {
             +"      Author: Ew0345\n"
             +"      Version: $version\n"
             +"\n"
-            +"How to use: StringGenKT [<Numbers of strings to " +
+            +"How to use: StringGenKT [<Write To File " +
+            "[yes|y|true|1]|[no|n|false|0]> <Numbers of strings to " +
             "generator> <Length of strings> <String type (1-6)>]\n"
             +"--Random String Generator--")
 }
@@ -64,30 +68,58 @@ fun pickChar(type:Int): String {
         3 -> char = symbols[Random.nextInt(0, symbols.size)]
         else -> {
             println("Selection error.\nPlease report issue on " +
-                    "<placeholder> with the title: \'Type " +
+                    "https://github.com/ew0345/RandomStringGenerator/issues with the title: \'Type " +
                     "Error: type was $type\'.")
         }
     }
     return char
 }
 
-fun generateString(stringAmount:Int, stringLength:Int,
+fun generateString(writeToFile:String, stringAmount:Int,
+                   stringLength:Int,
                    stringType:Int) {
-    for (i in 1..stringAmount) {
-        print("String #$i: ")
-        for (i1 in 1..stringLength) {
-            val type:Int = pickType(stringType)
-            val char:String = pickChar(type)
+    when (writeToFile.lowercase()) {
+        "true","yes","y","1" -> {
+            val stringFile = File(System.getProperty("user.home")
+                    +"/Desktop/RandomStrings.txt")
+            BufferedWriter(FileWriter(stringFile.absolutePath)).use { bw ->
+                for (i in 1..stringAmount) {
+                    print("String #$i: ")
+                    bw.write("String #$i: ")
+                    for (i1 in 1..stringLength) {
+                        val type: Int = pickType(stringType)
+                        val char: String = pickChar(type)
 
-            if (char == "oh no") exitProcess(0)
+                        if (char == "oh no") exitProcess(0)
 
-            print(char)
+                        print(char)
+                        bw.write(char)
+                    }
+                    print("\n")
+                    bw.newLine()
+                }
+            }
         }
-        print("\n")
+        "false","no","n","0" -> {
+            for (i in 1..stringAmount) {
+                print("String #$i: ")
+                for (i1 in 1..stringLength) {
+                    val type:Int = pickType(stringType)
+                    val char:String = pickChar(type)
+
+                    if (char == "oh no") exitProcess(0)
+
+                    print(char)
+                }
+                print("\n")
+            }
+        }
     }
 }
 
 fun getUserInput() {
+    print("Write to File: ")
+    val writeToFile = readLine().toString()
     print("String Amount: ")
     val stringAmount = Integer.valueOf(readLine())
     print("String Length: ")
@@ -95,14 +127,16 @@ fun getUserInput() {
     print("String Type: ")
     val stringType = Integer.valueOf(readLine())
 
-    generateString(stringAmount, stringLength, stringType)
+    generateString(writeToFile, stringAmount, stringLength,
+        stringType)
 }
 
 fun main(args: Array<String>) {
-    if (args.size == 3) {
-        val strGenInf = arrayOf(Integer.parseInt(args[0]),
-            Integer.parseInt(args[1]), Integer.parseInt(args[2]))
-        generateString(strGenInf[0], strGenInf[1], strGenInf[2])
+    if (args.size == 4) {
+        val strGenInf = arrayOf(Integer.parseInt(args[1]),
+            Integer.parseInt(args[2]), Integer.parseInt(args[3]))
+        generateString(args[0].toString(), strGenInf[0],
+            strGenInf[1], strGenInf[2])
     } else if (args.isNotEmpty() && args[0] == "--info"
         || args.isNotEmpty() && args[0] == "/?") {
         displayInfo()
