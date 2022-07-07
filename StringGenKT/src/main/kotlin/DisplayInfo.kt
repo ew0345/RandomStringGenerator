742ac73ba37320927bb2b6e073ebda106ba2454b
ew0345 <ew0345@users.noreply.github.com>
2022-07-07 03:40:53 -0400

    Added else to when statements
    I'm dumb and didn't add an else to the when statements for stringType
    
    And since I did that I upped the version number to 1.0.1-KT

--- StringGenKT/src/main/kotlin/DisplayInfo.kt
+++ StringGenKT/src/main/kotlin/DisplayInfo.kt
@@ -1,11 +1,10 @@
-package stringGenKT.src.main.kotlin
 /*
 * Author: Ew0345
 */
 
 class DisplayInfo {
     private val author = "Ew0345"
-    private val version = "1.1.0-KT"
+    private val version = "1.0.1-KT"
 
     fun main() {
         println(
@@ -21,22 +20,10 @@ class DisplayInfo {
             +"      5: Base symbols only\n"
             +"      6: Modifier symbols only (Symbols that require holding shift to type)\n"
             +"      7: Symbols only (Both base and modifier symbols)\n"
-            +"      8: Uppercase letters & numbers\n"
-            +"      9: Lowercase letters & numbers\n"
-            +"      10: Mixed-case letters & numbers\n"
-            +"      11: Uppercase letters & base symbols\n"
-            +"      12: Uppercase letters & modifier symbols\n"
-            +"      13: Uppercase letters & symbols\n"
-            +"      14: Lowercase letters & base symbols\n"
-            +"      15: Lowercase letters & modifier symbols\n"
-            +"      16: Lowercase letters & symbols\n"
-            +"      17: Mixed-case letters & base symbols \n"
-            +"      18: Mixed-case letters & modifier symbols\n"
-            +"      19: Mixed-case letters & symbols\n"
-            +"      20: Numbers & base symbols\n"
-            +"      21: Numbers & modifier symbols\n"
-            +"      22: Numbers & symbols\n"
-            +"      23: All\n"
+            +"      8: Mixed-case letters & numbers\n"
+            +"      9: Mixed-case letters & symbols\n"
+            +"      10: Numbers & symbols\n"
+            +"      11: All\n"
             +"---------------------------"
         )
     }
